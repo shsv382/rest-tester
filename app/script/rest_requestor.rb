9add06bc4c9afac85cb1e5755eced85b9a18b0af
shsv382 <shsv382@gmail.com>
2017-02-09 22:19:15 +0400

    Added Users controller and index-view

---
+++ app/script/rest_requestor.rb
@@ -0,0 +1,8 @@
+# app/script/rest_requestor.rb
+require 'rest-client'
+
+puts "Input URL"
+puts "(http://localhost:3000 - default)"
+url_input = gets.chomp
+url_input.empty? ? url = 'http://localhost:3000' : url = url_input
+puts RestClient.get(url)
